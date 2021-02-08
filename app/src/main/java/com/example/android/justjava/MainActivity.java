bb30c1cdd1146e407cad7d5a26b48d7dfdfec01c
Rezenee <zane.rickert@gmail.com>
2021-02-08 12:37:07 -0500

    14. Add Dummy add and subtract buttons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -34,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
+
+    public void increment(View view){
+        int quantity = 1;
+        display(3);
+        displayPrice(15);
+    }
+
+    public void decrement(View view){
+        int quantity = 1;
+        display(1);
+        displayPrice(5);
+    }
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
